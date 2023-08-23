173b971563b96c6efb5cd961bdb2e8208842ac0a
HowTech StuffWorks <howtechstuffworks@gmail.com>
2023-08-22 20:33:39 -0400

    MYDEV-1 - Unique ptr example.

--- 0_CPP_LEARN/0_CPP11/1_SmartPointers/1_WorkExamples/0_SharedPtr/1_UniquePtr.cpp
+++ 0_CPP_LEARN/0_CPP11/1_SmartPointers/1_WorkExamples/0_SharedPtr/1_UniquePtr.cpp
@@ -1,10 +1,31 @@
 #include <iostream>
+#include <memory>
 
 class A {
-private:
-  int size_;
+public:
+  A() { std::cout<<"Constructor called"<<std::endl; }
+  ~A() { std::cout<<"Destructor called"<<std::endl; }
+  void show() { std::cout<<"A::show()"<<std::endl; }
 };
+
+void foo(std::unique_ptr<A> foo_ptr) {
+  foo_ptr->show();
+  std::cout<<"Foo ptr:"<<foo_ptr.get()<<std::endl;
+}
 
 int main() {
   std::cout<<"Unique ptr"<<std::endl;
+
+  std::unique_ptr<A> p1(new A());
+  p1->show();
+  std::cout<<"P1: "<<p1.get()<<std::endl;
+
+  std::unique_ptr<A> p2 = std::move(p1);
+  p2->show();
+  std::cout<<"P1: "<<p1.get()<<std::endl;
+  std::cout<<"P2: "<<p2.get()<<std::endl;
+
+  foo(std::move(p2));
+  std::cout<<"P1: "<<p1.get()<<std::endl;
+  std::cout<<"P2: "<<p2.get()<<std::endl;
 }
